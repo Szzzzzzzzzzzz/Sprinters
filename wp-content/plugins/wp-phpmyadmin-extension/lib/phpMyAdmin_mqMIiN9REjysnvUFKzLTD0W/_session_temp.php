f5da0b7466087d9e6c8309e8f22f93ecaa8bdd6f
Danny <dan@liudandeMacBook-Pro.local>
2020-08-29 21:41:36 +1000

    Version 1 of 26/08

---
+++ wp-content/plugins/wp-phpmyadmin-extension/lib/phpMyAdmin_mqMIiN9REjysnvUFKzLTD0W/_session_temp.php
@@ -0,0 +1,1 @@
+<?php $sess_vars = ["time"=>1598420021, "name"=>"wp_pma_SZCO6slPgvTw7x",  "value"=>"wp_pma_gQCDtVj6B3haGlPOTX47NZU",  "require_ip"=>true, "ip"=>"60.224.71.199", "strip_slashes"=>true];
